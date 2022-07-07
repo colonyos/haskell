3e85555bd30a711e08de98b0f1edb46f2b6b545f
Johan Kristiansson <kristiansson.johan@gmail.se>
2022-07-07 11:39:37 +0200

    working cryptolib

--- app/Main.hs
+++ app/Main.hs
@@ -3,4 +3,13 @@ module Main where
 import CryptoLib
 
 main :: IO ()
-main = print_prvkey
+main = do k <- prvkey
+          i <- identity "d6eb959e9aec2e6fdc44b5862b269e987b8a4d6f2baca542d8acaa97ee5e74f6"
+          h <- hash "hello"
+          s <- sign h k
+          s2 <- sign "hello" "d6eb959e9aec2e6fdc44b5862b269e987b8a4d6f2baca542d8acaa97ee5e74f6"
+          print i
+          print k
+          print h
+          print s
+          print s2
--- src/CryptoLib.hs
+++ src/CryptoLib.hs
@@ -1,5 +1,5 @@
 module CryptoLib
-    ( print_prvkey 
+    ( prvkey, identity, sign, hash, recoverid
     ) where
 
 {-# LANGUAGE ForeignFunctionInterface #-}
@@ -8,12 +8,49 @@ import Foreign.C.Types
 import Foreign.C.String
 
 foreign import ccall "prvkey" c_prvkey :: IO CString
+foreign import ccall "id" c_identity :: CString -> IO CString
+foreign import ccall "sign" c_sign :: CString -> CString -> IO CString
+foreign import ccall "hash" c_hash :: CString -> IO CString
+foreign import ccall "recoverid" c_recoverid:: CString -> CString -> IO CString
 
+prvkey :: IO String
 prvkey = do k <- c_prvkey
-            res <- peekCString k
+            str <- peekCString k
             _ <- free k
-            return res
+            return str 
 
-print_prvkey :: IO ()
-print_prvkey = do k <- prvkey
-                  print $ k
+identity :: String -> IO String
+identity prvkey = do prvkeyc <- newCString prvkey 
+                     identityc <- c_identity prvkeyc 
+                     identity <- peekCString identityc 
+                     _ <- free identityc 
+                     _ <- free prvkeyc 
+                     return identity 
+
+sign :: String -> String -> IO String
+sign msg prvkey = do msgc <- newCString msg
+                     prvkeyc <- newCString prvkey
+                     sigc <- c_sign msgc prvkeyc
+                     sig <- peekCString sigc
+                     _ <- free sigc
+                     _ <- free msgc
+                     _ <- free prvkeyc
+                     return sig
+
+hash :: String -> IO String
+hash msg = do msgc <- newCString msg
+              hashc <- c_hash msgc
+              hash <- peekCString hashc 
+              _ <- free hashc
+              _ <- free msgc
+              return hash 
+
+recoverid :: String -> String -> IO String
+recoverid msg sig = do msgc <- newCString msg
+                       sigc <- newCString sig
+                       identityc <- c_recoverid msgc sigc
+                       identity <- peekCString identityc 
+                       _ <- free identityc 
+                       _ <- free msgc
+                       _ <- free sigc
+                       return identity 
